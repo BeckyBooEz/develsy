0c71d2b4ae8f22659c7a02ac5b62f4a4f3418c8a
Arturo Miranda <arturmiranda917@gmail.com>
2026-02-28 18:09:17 -0600

    Editar estilos globales creación ruta spotifyTop

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>Pagína de inicio Develsy</h1>
+      <Link href="/spotifytop">Spotify Top</Link>
     </main>
   );
 }
---
+++ app/spotifytop/page.tsx
@@ -0,0 +1,7 @@
+export default function spotifytop(){
+    return(
+        <main>
+            <h1>Spotify Top</h1>
+        </main>
+    )
+}
