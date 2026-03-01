cac0f69b4a5ba011616566585e8b2621edd5058b
Arturo Miranda <arturmiranda917@gmail.com>
2026-02-28 23:56:08 -0600

    Ajuste de margin del header Spotify-top

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,12 +2,12 @@ import Image from "next/image"
 
 export default function Header() {
     return (
-        <header className="flex flex-row justify-around items-center">
+        <header className="flex flex-row justify-between items-center m-2">
             <Image src="/img/spotifylogo.svg" alt="SpotifyLogo" width={200} height={0} />
             <h1 className="text-3xl">Aprendiendo TSX</h1>
             <div className="flex flex-row items-center gap-2.5">
                 <p className="text-lg">Arturo Miranda</p>
-                <Image src="/img/perfilarturo.webp" alt="ArturoPerfil" width={80} height={80} />
+                <Image className="rounded-full" src="/img/perfilarturo.webp" alt="ArturoPerfil" width={80} height={80} />
             </div>
         </header>
     )
