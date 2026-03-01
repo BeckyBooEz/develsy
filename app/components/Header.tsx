2e6858bb5a0d6b0344c7de28a3f9cd62c3028856
Arturo Miranda <arturmiranda917@gmail.com>
2026-03-01 00:24:31 -0600

    Responsibe, alloweddevorigins

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,10 +2,10 @@ import Image from "next/image"
 
 export default function Header() {
     return (
-        <header className="flex flex-row justify-between items-center m-2">
+        <header className="flex flex-col sm:flex-row justify-between items-center m-2">
             <Image src="/img/spotifylogo.svg" alt="SpotifyLogo" width={200} height={0} />
-            <h1 className="text-3xl">Aprendiendo TSX</h1>
-            <div className="flex flex-row items-center gap-2.5">
+            <h1 className="text-3xl mt-2 sm:mt-0">Aprendiendo TSX</h1>
+            <div className="flex flex-row items-center gap-2.5 mt-2 sm:mt-0">
                 <p className="text-lg">Arturo Miranda</p>
                 <Image className="rounded-full" src="/img/perfilarturo.webp" alt="ArturoPerfil" width={80} height={80} />
             </div>
