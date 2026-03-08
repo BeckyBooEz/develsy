00a2f4a1ef4642c16c30df45c40b3c6f2fcbe33f
Arturo Miranda <arturmiranda917@gmail.com>
2026-03-08 14:20:01 -0600

    Cambiar color link pagina de inicio

--- app/Becky/page.tsx
+++ app/Becky/page.tsx
@@ -4,7 +4,6 @@ import { miCV } from "../data/micv";
 export default function pruebasbecky() {
     return (
         <div>
-            <div>Hola</div>
             <CurriculumArturo miCV={miCV} />
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1>Pagína de inicio Develsy</h1>
+      <Link className="bg-sky-500" href={"/Becky"}>BeckyBooEz</Link>
     </main>
   );
 }
