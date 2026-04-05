56100d622ffbf08df9784a4f8b71f077585b2563
Arturo Miranda <arturmiranda917@gmail.com>
2026-04-04 18:46:10 -0600

    Ponerle foto profesional

--- app/beckybooez/_curriculum/CurriculumPerfil.tsx
+++ app/beckybooez/_curriculum/CurriculumPerfil.tsx
@@ -10,7 +10,7 @@ export default function CurriculumPerfil({ perfil }: Props) {
             <div className="w-full md:w-1/3 flex justify-center">
                 <img
                     className="rounded-full"
-                    src={"/img/perfil.png"}
+                    src={"/img/perfilprofesional.png"}
                     alt="FotoPerfil"
                     width={400}
                     height={400}
