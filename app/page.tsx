bc2d500c92c461b72e28276dd81410ba69782f4d
Arturo Miranda <arturmiranda917@gmail.com>
2026-05-30 13:29:34 -0600

    Eliminar links a proyectos personales

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Pagína de inicio Develsy</h1>
-      <div>
-        <Link className="underline" href={"/spotifytop"}>Spotify Top</Link>
-      </div>
-      <div>
-        <Link className="underline" href={"/beckybooez"}>BeckyBooEz</Link>
-      </div>
-      <div>
-        <Link className="underline" href={"/recentlyplayed"}>RecientesSpotify</Link>
-      </div>
-      <div>
-        <Link className="underline" href={"/nomina"}>Analizador de Nomina</Link>
+    <main className="flex-col gap-3">
+      <h1 className="text-3xl">Pagína de inicio Develsy</h1>
+      <div className="text-2xl bg-purple-300 text-cyan-600">
+        <Link className="underline" href={"/beckybooez"}>Arturo Miranda</Link>
       </div>
     </main>
   );
