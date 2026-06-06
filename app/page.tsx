4946ea9366e6ceb8cd32c6cd8892aedeb04b9113
Arturo Miranda <arturmiranda917@gmail.com>
2026-06-06 14:15:03 -0600

    ArturoMirandaCV

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-slate-50 px-6">
@@ -7,10 +9,12 @@ export default function Home() {
           Develsy
         </h1>
 
+        <Link href="/ArturoMiranda">Arturo Miranda</Link>
+
         <div className="mt-10 text-sm text-slate-400">
           © {new Date().getFullYear()} Develsy
         </div>
       </div>
-    </main>
+    </main >
   );
 }
